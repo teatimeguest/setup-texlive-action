df9c0d2d63d9435af5475cae5d042206589107ab
teatimeguest <teatimeguest@gmail.com>
2021-12-11 12:56:43 +0900

    chore: add a release script [skip ci]

---
+++ .github/workflows/release.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+
+function usage() {
+  cat <<EOF >&2
+Create a release commit with tags
+
+USAGE:
+    $(basename "$0") {patch|minor|major}
+EOF
+}
+
+function validate() {
+  if [[ "$#" -ne 1 || ! "$1" =~ ^(patch|minor|major)$ ]]; then
+    usage; exit 1
+  fi
+}
+
+function release() {
+  [[ "$(git rev-parse --abbrev-ref HEAD)" == main ]]
+  git --no-pager diff --exit-code --staged
+  git --no-pager diff --exit-code -- package-lock.json package.json
+
+  local -r next="$(npm version "$1")"
+  git add package-lock.json package.json
+  git commit -m "chore(release): prepare for ${next}"
+  git chglog --next-tag "${next}" | git tag "${next}" --cleanup=whitespace -F -
+  git tag -f "${next%%.*}" -m "${next}"
+
+  git --no-pager show "${next}"
+}
+
+validate "$@"
+release "$@"
